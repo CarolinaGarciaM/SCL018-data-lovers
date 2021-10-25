a34b1fecc4b6e60d7a223bb6409556a43ec961d5
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2021-10-25 19:35:50 -0300

    Imágenes de slider

--- src/data.js
+++ src/data.js
@@ -1,9 +1,49 @@
 // estas funciones son de ejemplo
-
+/* 
 export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+}; */
+
+export const sortData = (data, sortBy, sortOrder) => {
+
+  const dataGhibli = data.films;
+
+  const movieSort = dataGhibli.sort(function (a, b) {
+
+    if (a [sortBy] > b [sortBy]) {
+      return 1;
+    }
+    if (a [sortBy] < b [sortBy]) {
+      return -1;
+    }
+    return 0;
+  });
+  
+  if (sortOrder === "rt_score") {
+    return movieSort.reverse();
+  }
+ /*  else if (sortOrder === "release_date") {
+    return movieSort.reverse
+  }   */
+    return movieSort;
+  }  
+
+  export const filterData = (data, condition) => {
+
+    const infoGhibli = data.films;
+   
+     const movieFilter = infoGhibli.filter(function (films){
+   
+       return films.director === "Hayao Miyazaki";
+     
+      //también funciona: return films.director.includes("Hayao Miyazaki");
+      
+   
+     });
+     return movieFilter; 
+     
+     };
--- src/main.js
+++ src/main.js
@@ -1,23 +1,102 @@
-import { example } from "./data.js";
-// import data from './data/lol/lol.js';
+/*IMPORTACIÓN DE MÓDULOS*/
 import data from "./data/ghibli/ghibli.js";
-// import data from './data/rickandmorty/rickandmorty.js';
+import {sortData} from "./data.js";
+import {filterData} from "./data.js";
 
-let films = data.films;
-let printMovie = document.getElementById("posterFilms");
+/*SLIDER*/
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slider");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+/*ESTABLECIMIENTO DE VARIABLES*/
+const films = data.films; 
+// variable que contienen las películas de la data.
+const printedMovies = document.getElementById("posterFilms"); 
+// variable que contiene la section en html.
+let newFilms = [...films];
+// variable que contiene una copia de las películas de la data.
+
+/*CONFIGURACIÓN DE LAS TARJETAS QUE SE MOSTRARÁN EN EL HTML*/
 const drawCard = (films) => {
   return `
      <section class="movieCard">
-     <div class="movieFrame">
+     <div class="flip-card">
+     <div class="flip-card-inner">
+       <div class="flip-card-front">
+
         <img class= "moviePoster" alt="Poster de la película" id="poster" src="${films.poster}">
         <figcaption class="movieName">${films.title}</figcaption>
         <figcaption class="movieYear">(${films.release_date})</figcaption>
+        <figcaption class="movieScore">${films.rt_score}</figcaption>
         </div>
+        <div class="flip-card-back"
+        <h3> ${films.description}<h3>
+        </div>
+        </div>
+      </div>
+
      </section>`;
 };
 
+// FUNCIÓN QUE IMPRIME LAS TARJETAS CON LAS PELÍCULAS
+    const displayMovies = (filmsSelected) => {
+     printedMovies.innerHTML ="";
+     filmsSelected.forEach((newFilms) => {
+        printedMovies.innerHTML += drawCard(newFilms);
+     })
+     
+    }
+    displayMovies(newFilms);
+
+/* FUNCIÓN QUE IMPRIME CARTAS CON UN CICLO FOR
 for (let i = 0; i < films.length; i++) {
   printMovie.innerHTML += drawCard(films[i]);
-}
-const filterByGender = document.querySelector("#filterByGender");
+} */
+
+// INSTRUCCIONES PARA IMPRIMIR CARTAS ORDENADAS
+const orderSelected = document.querySelector(".combo-box-order");
+
+orderSelected.addEventListener("change", (event) => {
+  const chosenOrder = sortData(data, event.target.value, event.target.value);
+  const print = (newFilms) => {
+    displayMovies(newFilms);
+  }
+  
+  print(chosenOrder);
+});
+
+// INSTRUCCIONES PARA IMPRIMIR CARTAS FILTRADAS
+const filterSelected = document.querySelector(".combo-box-filter");
+
+filterSelected.addEventListener("change", (event) => {
+const chosenFilter = filterData(data, event.target.value);
+const print = (newFilms) => {
+  displayMovies(newFilms);
+  }
+
+   print(chosenFilter);
+});
+
+/* data.films.forEach(function(e){
+
+  const females = e.people.filter( (element) =>
+  element.gender === "Female");
+  console.log(females);
+}); */
+
+
